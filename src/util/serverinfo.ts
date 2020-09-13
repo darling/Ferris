e80dd5f7c33f0d0b85699634a512fb67950bfb45
Carter Black <carterb@mailbox.org>
2020-09-13 01:28:39 -0700

    new serverConfigs to replace servers

--- src/util/serverinfo.ts
+++ src/util/serverinfo.ts
@@ -1,7 +1,6 @@
 import Enmap from 'enmap'
 
-const servers = new Map();
 const pendingUnpunishments = new Map();
 const serverConfigs = new Enmap()
 
-export { servers, pendingUnpunishments, serverConfigs };
+export { pendingUnpunishments, serverConfigs };
