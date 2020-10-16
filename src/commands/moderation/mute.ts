2cb3f2e5538f78af3aae0041d92533017fd9f1c2
Carter B <lovable@riseup.net>
2020-10-15 22:16:12 -0700

    mute

--- src/commands/moderation/mute.ts
+++ src/commands/moderation/mute.ts
@@ -5,6 +5,78 @@ import { isUserMention, parseIfTime, stripMention } from '../../util/parse';
 import { muteDialog } from '../../util/muteFunctions';
 import { sendSimpleEmbed } from '../../util/embed';
 
+import { IDatabaseSchema } from '../../util/databaseFunctions';
+import { serverConfigs } from '../../util/serverInfo';
+
+import { client } from '../../app';
+
+client.commands.set('warn', {
+    name: 'warn',
+    guildOnly: true,
+    arguments: [
+        {
+            name: 'user',
+            type: 'member',
+            required: true,
+        },
+        {
+            name: 'time',
+            type: 'duration',
+            required: true,
+        },
+        {
+            name: 'reason',
+            type: '...string',
+            required: false,
+        },
+    ],
+    run: (msg, args: PunishArgs, guild) => {
+        if (!guild) return;
+
+        const muteRole = guild.roles.cache.find((role) => role.name === 'muted');
+        if (!muteRole) return;
+
+        let currentRoles = args.user.roles.cache
+            .array()
+            .map((role) => role.id)
+            .toString();
+
+        args.user.roles.set([muteRole]).then((mutedMember) => {
+            muteDialog(mutedMember, args.time, msg);
+        });
+
+        let time = admin.firestore.Timestamp.fromMillis(Date.now() + args.time);
+        let timeGiven = admin.firestore.Timestamp.now();
+
+        let docData = {
+            guild: guild.id,
+            channel: msg.channel.id,
+            completed: false,
+            desc: '',
+            type: 'mute',
+            roles: currentRoles,
+            timeGiven: timeGiven,
+            author: msg.author.id,
+            time: time,
+        };
+
+        let document = firestore
+            .collection('guilds')
+            .doc(guild.id)
+            .collection('punishments')
+            .doc(args.user.id);
+
+        msg.channel.send(`\`\`\`json\n${JSON.stringify(docData, null, 2)}\`\`\``);
+        document.set(docData);
+    },
+});
+
+interface PunishArgs {
+    user: GuildMember;
+    time: number;
+    reason?: string;
+}
+
 // const run: RunCommand = function (client: FerrisClient, msg: Message, args: string[]): void {
 //     const guild: Guild | null = msg.guild;
 //     if (guild === null) {
@@ -54,10 +126,6 @@ import { sendSimpleEmbed } from '../../util/embed';
 //         .array()
 //         .map((role) => role.id)
 //         .toString();
-
-//     mutedMember.roles.set([muteRole]).then((mutedMember) => {
-//         muteDialog(mutedMember, timeSpecified, msg);
-//     });
 
 //     let time = admin.firestore.Timestamp.fromMillis(Date.now() + timeSpecified);
 //     let timeGiven = admin.firestore.Timestamp.now();
