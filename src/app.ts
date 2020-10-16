47f375bfad3cb5eca52dc78337950311eb43bae9
Carter B <lovable@riseup.net>
2020-10-15 21:56:45 -0700

    env vars

--- src/app.ts
+++ src/app.ts
@@ -5,12 +5,37 @@ import * as admin from 'firebase-admin';
 
 // Util
 import { readdir, readdirSync } from 'fs';
+import { IConfig } from './assets/config/config';
 
-import { config } from './assets/config/config';
+// import { config } from './assets/config/config';
 import { ICommand } from './types/commands';
 
+const type = process.env.TYPE;
+const project_id = process.env.PROJECT_ID;
+const private_key_id = process.env.PRIVATE_KEY_ID;
+const private_key = process.env.PRIVATE_KEY;
+const client_email = process.env.CLIENT_EMAIL;
+const client_id = process.env.CLIENT_ID;
+const curl = process.env.client_x509_cert_url;
+
+if (!type || !project_id || !private_key || !private_key_id || !client_email || !client_id || !curl)
+    throw Error('Load console vargs');
+
+const envKey = {
+    type: type,
+    project_id: project_id,
+    private_key_id: private_key_id,
+    private_key: private_key,
+    client_email: client_email,
+    client_id: client_id,
+    auth_uri: 'https://accounts.google.com/o/oauth2/auth',
+    token_uri: 'https://oauth2.googleapis.com/token',
+    auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
+    client_x509_cert_url: curl,
+};
+
 admin.initializeApp({
-    credential: admin.credential.cert(config.firebase),
+    credential: admin.credential.cert(JSON.stringify(envKey)),
     databaseURL: 'https://ferrisbot-6e0f1.firebaseio.com/',
 });
 
@@ -46,4 +71,4 @@ require(__dirname + '/commands');
 export { client, FerrisClient, db, firestore, admin };
 
 // Let's start!
-client.login(config.token);
+client.login(process.env.BOT_TOKEN);
--- src/assets/config/config.ts
+++ src/assets/config/config.ts
@@ -1,8 +1,7 @@
 import { ServiceAccount } from 'firebase-admin';
+import { config } from 'process';
 
 export interface IConfig {
     token: string;
     firebase: ServiceAccount | string;
 }
-
-export const config: IConfig = require('./../../../config.json');
