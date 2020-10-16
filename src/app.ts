70fb777ffc205e854b91609f994fb82086d19154
Carter B <lovable@riseup.net>
2020-10-15 21:59:55 -0700

    fixes

--- src/app.ts
+++ src/app.ts
@@ -21,21 +21,14 @@ const curl = process.env.client_x509_cert_url;
 if (!type || !project_id || !private_key || !private_key_id || !client_email || !client_id || !curl)
     throw Error('Load console vargs');
 
-const envKey = {
-    type: type,
-    project_id: project_id,
-    private_key_id: private_key_id,
-    private_key: private_key,
-    client_email: client_email,
-    client_id: client_id,
-    auth_uri: 'https://accounts.google.com/o/oauth2/auth',
-    token_uri: 'https://oauth2.googleapis.com/token',
-    auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
-    client_x509_cert_url: curl,
+const envKey: admin.ServiceAccount = {
+    projectId: project_id,
+    privateKey: private_key,
+    clientEmail: client_email,
 };
 
 admin.initializeApp({
-    credential: admin.credential.cert(JSON.stringify(envKey)),
+    credential: admin.credential.cert(envKey),
     databaseURL: 'https://ferrisbot-6e0f1.firebaseio.com/',
 });
 
