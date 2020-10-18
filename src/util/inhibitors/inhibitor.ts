d02cf38aafc0619d90d9e22934e6f69fd3d315ac
Carter Black <carterb@mailbox.org>
2020-10-17 20:22:56 -0700

    added inhibitor collection

---
+++ src/util/inhibitors/inhibitor.ts
@@ -0,0 +1,7 @@
+import { Collection, Guild, Message } from 'discord.js';
+import { ICommand } from '../../types/commands';
+
+export const inhibitors = new Collection<
+    string,
+    (msg: Message, command: ICommand, guild?: Guild) => Promise<boolean>
+>();
